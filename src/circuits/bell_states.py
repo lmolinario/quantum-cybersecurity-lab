62feee418b0fd12c8c886c21a29885da5354d5e8
Lello Molinario <114803243+lmolinario@users.noreply.github.com>
2026-05-01 22:16:33 +0200

    Add Bell-state circuit builders

---
+++ src/circuits/bell_states.py
@@ -0,0 +1,47 @@
+"""
+Bell-state circuit builders.
+
+Bell states are among the simplest examples of quantum entanglement.
+"""
+
+from qiskit import QuantumCircuit
+
+
+def bell_phi_plus() -> QuantumCircuit:
+    """Create the Bell state |Phi+> = (|00> + |11>) / sqrt(2)."""
+    circuit = QuantumCircuit(2, 2)
+    circuit.h(0)
+    circuit.cx(0, 1)
+    circuit.measure([0, 1], [0, 1])
+    return circuit
+
+
+def bell_phi_minus() -> QuantumCircuit:
+    """Create the Bell state |Phi-> = (|00> - |11>) / sqrt(2)."""
+    circuit = QuantumCircuit(2, 2)
+    circuit.h(0)
+    circuit.cx(0, 1)
+    circuit.z(0)
+    circuit.measure([0, 1], [0, 1])
+    return circuit
+
+
+def bell_psi_plus() -> QuantumCircuit:
+    """Create the Bell state |Psi+> = (|01> + |10>) / sqrt(2)."""
+    circuit = QuantumCircuit(2, 2)
+    circuit.h(0)
+    circuit.cx(0, 1)
+    circuit.x(1)
+    circuit.measure([0, 1], [0, 1])
+    return circuit
+
+
+def bell_psi_minus() -> QuantumCircuit:
+    """Create the Bell state |Psi-> = (|01> - |10>) / sqrt(2)."""
+    circuit = QuantumCircuit(2, 2)
+    circuit.h(0)
+    circuit.cx(0, 1)
+    circuit.x(1)
+    circuit.z(0)
+    circuit.measure([0, 1], [0, 1])
+    return circuit
